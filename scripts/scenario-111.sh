7e529e790257e8191a56aa33d561761f7766a580
Daniel Rene Bayer <daniel.bayer@fau.de>
2021-04-13 14:36:03 +0200

    Scenario-scripts updated

--- scripts/scenario-111.sh
+++ scripts/scenario-111.sh
@@ -3,29 +3,30 @@
 cd $(dirname $0)
 
 datestr=$(date +"%Y%m%d-%H%M")
-checkpoint_dir=$(realpath "../checkpoints/s109")"/${datestr}"
+checkpoint_dir=$(realpath "../checkpoints/s111")"/${datestr}"
 
 mkdir -p $checkpoint_dir
 
-for i in $(seq 170); do
+for i in $(seq 140); do
     python ../code/TrainingController.py \
 	--algorithm ddqn \
-	--model "Building_5ZoneAirCooled_SingleSetpoint_SmallAgents" \
+	--model Building_5ZoneAirCooled_SingleSetpoint_SmallAgents \
 	--ts_per_hour 1 \
+	--eplus_storage_mode \
 	--lr 0.001 \
 	--discount_factor 0.91 \
 	--batch_size 256 \
 	--use_cuda \
 	--episodes_count 80 \
-	--reward_function "rulebased_roomtemp" \
+	--reward_function "rulebased_agent_output" \
 	--network_storage_frequency 80 \
 	--epsilon 0.05 \
-	--epsilon_final_step 13000 \
-	--agent_w_l2 0.000003 \
+	--epsilon_final_step 11000 \
+	--agent_w_l2 0.000002 \
 	--checkpoint_dir $checkpoint_dir \
 	--epw_file ../../COBS/cobs/data/weathers/8.epw \
 	--episode_start_month 1 \
-	--continue_training | sed -n -e '/^Ep.*/p' -e '/^Lo.*/p'
+	--continue_training #| sed -n -e '/^Ep.*/p' -e '/^Lo.*/p'
     if ! (( $? == 0 )); then
 	break
 	echo "Error during scenario run."
