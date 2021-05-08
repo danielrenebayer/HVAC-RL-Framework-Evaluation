46f90ba6e8cead857a4eb0b4b56f1ba31f938637
Daniel Rene Bayer <daniel.bayer@fau.de>
2021-05-08 15:12:10 +0200

    Scripts for scenarios 131, 132, 133, 134 introduced

--- scripts/scenario-132.sh
+++ scripts/scenario-132.sh
@@ -4,31 +4,31 @@ set -o pipefail
 cd $(dirname $0)
 
 datestr=$(date +"%Y%m%d-%H%M")
-checkpoint_dir=$(realpath "../checkpoints/s131")"/${datestr}"
+checkpoint_dir=$(realpath "../checkpoints/s132")"/${datestr}"
 
 mkdir -p $checkpoint_dir
 
-for i in $(seq 350); do
+for i in $(seq 280); do
     python ../code/TrainingController.py \
 	--algorithm ddqn \
 	--model "Building_5ZoneAirCooled_SingleSetpoint_SingleSmallAgent" \
 	--ts_per_hour 1 \
 	--eplus_storage_mode \
-	--lr 0.075 \
-	--discount_factor 0.7 \
+	--lr 0.1 \
+	--discount_factor 0.8 \
 	--batch_size 512 \
 	--episodes_count 88 \
 	--reward_offset 0.5 \
 	--reward_scale 0.25 \
 	--stp_reward_function "linear" \
-	--stp_reward_step_offset 1.0 \
+	--stp_reward_step_offset 2.0 \
 	--reward_function "rulebased_agent_output" \
 	--network_storage_frequency 88 \
-	--target_network_update_freq 7 \
+	--target_network_update_freq 1 \
 	--epsilon 0.05 \
-	--epsilon_final_step 30000 \
+	--epsilon_final_step 24000 \
 	--agent_network "1HiddenBigLayer,SiLU" \
-	--agent_w_l2 0.0000005 \
+	--agent_w_l2 0.000001 \
 	--checkpoint_dir $checkpoint_dir \
 	--idf_file $(realpath 5ZoneAirCooled_HigherWinterSetpoint.idf) \
 	--epw_file ../../COBS/cobs/data/weathers/8.epw \
--- scripts/scenario-134.sh
+++ scripts/scenario-134.sh
@@ -4,31 +4,31 @@ set -o pipefail
 cd $(dirname $0)
 
 datestr=$(date +"%Y%m%d-%H%M")
-checkpoint_dir=$(realpath "../checkpoints/s131")"/${datestr}"
+checkpoint_dir=$(realpath "../checkpoints/s134")"/${datestr}"
 
 mkdir -p $checkpoint_dir
 
 for i in $(seq 350); do
     python ../code/TrainingController.py \
 	--algorithm ddqn \
-	--model "Building_5ZoneAirCooled_SingleSetpoint_SingleSmallAgent" \
+	--model "Building_5ZoneAirCooled_SingleSetpoint_SingleAgent" \
 	--ts_per_hour 1 \
 	--eplus_storage_mode \
-	--lr 0.075 \
-	--discount_factor 0.7 \
-	--batch_size 512 \
+	--lr 0.07 \
+	--discount_factor 0.25 \
+	--batch_size 256 \
 	--episodes_count 88 \
-	--reward_offset 0.5 \
+	--reward_offset 0.6 \
 	--reward_scale 0.25 \
 	--stp_reward_function "linear" \
-	--stp_reward_step_offset 1.0 \
+	--stp_reward_step_offset 1.7 \
 	--reward_function "rulebased_agent_output" \
 	--network_storage_frequency 88 \
-	--target_network_update_freq 7 \
+	--target_network_update_freq 6 \
 	--epsilon 0.05 \
 	--epsilon_final_step 30000 \
-	--agent_network "1HiddenBigLayer,SiLU" \
-	--agent_w_l2 0.0000005 \
+	--agent_network "2HiddenLayer,Trapezium,SiLU" \
+	--agent_w_l2 0.0000015 \
 	--checkpoint_dir $checkpoint_dir \
 	--idf_file $(realpath 5ZoneAirCooled_HigherWinterSetpoint.idf) \
 	--epw_file ../../COBS/cobs/data/weathers/8.epw \
