311f1332e7c10aa211ed02b097fdebdcc64ad1a0
Daniel Rene Bayer <daniel.bayer@fau.de>
2021-05-10 14:41:49 +0200

    scenarios updated

--- scripts/scenario-132.sh
+++ scripts/scenario-132.sh
@@ -8,18 +8,19 @@ checkpoint_dir=$(realpath "../checkpoints/s132")"/${datestr}"
 
 mkdir -p $checkpoint_dir
 
-for i in $(seq 280); do
+for i in $(seq 25); do
     python ../code/TrainingController.py \
 	--algorithm ddqn \
 	--model "Building_5ZoneAirCooled_SingleSetpoint_SingleSmallAgent" \
 	--ts_per_hour 1 \
 	--eplus_storage_mode \
-	--lr 0.1 \
+	--lr 0.55 \
+	--optimizer "rmsprop" \
 	--discount_factor 0.8 \
 	--batch_size 512 \
 	--episodes_count 88 \
-	--reward_offset 0.5 \
-	--reward_scale 0.25 \
+	--reward_offset 0.1 \
+	--reward_scale 0.125 \
 	--stp_reward_function "linear" \
 	--stp_reward_step_offset 2.0 \
 	--reward_function "rulebased_agent_output" \
@@ -33,6 +34,103 @@ for i in $(seq 280); do
 	--idf_file $(realpath 5ZoneAirCooled_HigherWinterSetpoint.idf) \
 	--epw_file ../../COBS/cobs/data/weathers/8.epw \
 	--episode_start_month 1 \
+	--continue_training | sed -n -e '/^Ep.*/p' -e '/^Lo.*/p'
+    if ! (( $? == 0 )); then
+	echo "Error during scenario run."
+	exit 1
+    fi
+done
+for i in $(seq 150); do
+    python ../code/TrainingController.py \
+	--algorithm ddqn \
+	--model "Building_5ZoneAirCooled_SingleSetpoint_SingleSmallAgent" \
+	--ts_per_hour 1 \
+	--eplus_storage_mode \
+	--lr 0.1 \
+	--optimizer "rmsprop" \
+	--discount_factor 0.8 \
+	--batch_size 512 \
+	--episodes_count 88 \
+	--reward_offset 0.1 \
+	--reward_scale 0.125 \
+	--stp_reward_function "linear" \
+	--stp_reward_step_offset 2.0 \
+	--reward_function "rulebased_agent_output" \
+	--network_storage_frequency 88 \
+	--target_network_update_freq 5 \
+	--epsilon 0.05 \
+	--epsilon_final_step 24000 \
+	--agent_network "1HiddenBigLayer,SiLU" \
+	--agent_w_l2 0.000001 \
+	--checkpoint_dir $checkpoint_dir \
+	--idf_file $(realpath 5ZoneAirCooled_HigherWinterSetpoint.idf) \
+	--epw_file ../../COBS/cobs/data/weathers/8.epw \
+	--episode_start_month 1 \
+	--continue_training | sed -n -e '/^Ep.*/p' -e '/^Lo.*/p'
+    if ! (( $? == 0 )); then
+	echo "Error during scenario run."
+	exit 1
+    fi
+done
+for i in $(seq 175); do
+    python ../code/TrainingController.py \
+	--algorithm ddqn \
+	--model "Building_5ZoneAirCooled_SingleSetpoint_SingleSmallAgent" \
+	--ts_per_hour 1 \
+	--eplus_storage_mode \
+	--lr 0.05 \
+	--optimizer "rmsprop" \
+	--discount_factor 0.8 \
+	--batch_size 512 \
+	--episodes_count 88 \
+	--reward_offset 0.1 \
+	--reward_scale 0.125 \
+	--stp_reward_function "linear" \
+	--stp_reward_step_offset 2.0 \
+	--reward_function "rulebased_agent_output" \
+	--network_storage_frequency 88 \
+	--target_network_update_freq 7 \
+	--epsilon 0.05 \
+	--epsilon_final_step 24000 \
+	--agent_network "1HiddenBigLayer,SiLU" \
+	--agent_w_l2 0.000001 \
+	--checkpoint_dir $checkpoint_dir \
+	--idf_file $(realpath 5ZoneAirCooled_HigherWinterSetpoint.idf) \
+	--epw_file ../../COBS/cobs/data/weathers/8.epw \
+	--episode_start_month 1 \
+	--continue_training | sed -n -e '/^Ep.*/p' -e '/^Lo.*/p'
+    if ! (( $? == 0 )); then
+	echo "Error during scenario run."
+	exit 1
+    fi
+done
+for i in $(seq 1); do
+    python ../code/TrainingController.py \
+	--algorithm ddqn \
+	--model "Building_5ZoneAirCooled_SingleSetpoint_SingleSmallAgent" \
+	--ts_per_hour 1 \
+	--eplus_storage_mode \
+	--lr 0.001 \
+	--optimizer "rmsprop" \
+	--discount_factor 0.8 \
+	--batch_size 512 \
+	--episodes_count 88 \
+	--reward_offset 0.1 \
+	--reward_scale 0.125 \
+	--stp_reward_function "linear" \
+	--stp_reward_step_offset 2.0 \
+	--reward_function "rulebased_agent_output" \
+	--network_storage_frequency 88 \
+	--target_network_update_freq 7 \
+	--epsilon 0.05 \
+	--epsilon_final_step 24000 \
+	--agent_network "1HiddenBigLayer,SiLU" \
+	--agent_w_l2 0.000001 \
+	--checkpoint_dir $checkpoint_dir \
+	--idf_file $(realpath 5ZoneAirCooled_HigherWinterSetpoint.idf) \
+	--epw_file ../../COBS/cobs/data/weathers/8.epw \
+	--episode_start_month 1 \
+	--output_Q_vals_iep \
 	--continue_training | sed -n -e '/^Ep.*/p' -e '/^Lo.*/p'
     if ! (( $? == 0 )); then
 	echo "Error during scenario run."
